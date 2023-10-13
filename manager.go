0ae0e709d6b036d702a44ddb7b0b511f6c89f68c
gastraderr <gaslimits@gmail.com>
2023-10-12 21:50:39 -0400

    first commit

--- manager.go
+++ manager.go
@@ -185,7 +185,7 @@ func (m *Manager) removeClient(client *Client) {
 func checkOrigin(r *http.Request) bool {
 	origin := r.Header.Get("Origin")
 	switch origin {
-	case "https://localhost:8080":
+	case "http://localhost:8080":
 		return true
 	default:
 		return false
